9dcaa76d1247f87710876b1a2a25690842ca3bd1
alo_wu <ilya.bela@yandex.ru>
2021-01-29 00:39:11 +0300

    add: replace method

--- src/main/java/com/epam/task/second/logic/StringMethods.java
+++ src/main/java/com/epam/task/second/logic/StringMethods.java
@@ -4,6 +4,9 @@ public class StringMethods implements TextProcessor {
     private final String removeAllNonWordSymbols = "[\\d+\\W ]";
     private final String insertSpaces = ".(?=.)";
     private final String spaces = "$0 ";
+    private final int replacePosition = 5;
+    private final String delimiter = " ";
+    private final char substitute = '_';
 
     @Override
     public String removeSymbols(String string) {
@@ -14,6 +17,18 @@ public class StringMethods implements TextProcessor {
 
     @Override
     public String replaceLetter(String string) {
-        return null;
+        String[] words = string.split(delimiter);
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            StringBuilder buffer = new StringBuilder(word);
+            if (word.length() >= replacePosition) {
+                buffer.setCharAt(replacePosition - 1, substitute);
+            }
+            result.append(buffer).append(delimiter);
+        }
+        // deleting last space
+        result.deleteCharAt(result.length() - 1);
+
+        return new String(result);
     }
 }
