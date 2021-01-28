b130760798dfb8ed3bd0d23fc8063fe0b681b755
alo_wu <ilya.bela@yandex.ru>
2021-01-29 00:12:28 +0300

    add: test for replace method

--- src/test/java/com/epam/task/second/logic/StringTest.java
+++ src/test/java/com/epam/task/second/logic/StringTest.java
@@ -5,12 +5,27 @@ import org.junit.Test;
 
 public class StringTest {
 
+    private final String TEST_REMOVE_INSERT = "he9 llo.wo7rl98d";
+    private final String EXPECTED_RI = "h e l l o w o r l d";
+
+    private final String TEST_REPLACE_K = "hello world text file to test classes";
+    private final String EXPECTED_RK = "hell_ worl_ text file to test clas_es";
+
     @Test
     public void testRemoveSymbolsShouldRemoveAllNonLiteralsAndInsertSpaces() {
-        StringMethods rep = new StringMethods();
+        StringMethods remove = new StringMethods();
 
-        String res = rep.removeSymbols("he9 llo.wo7rl98d");
+        String result = remove.removeSymbols(TEST_REMOVE_INSERT);
 
-        Assert.assertEquals("h e l l o w o r l d", res);
+        Assert.assertEquals(EXPECTED_RI, result);
+    }
+
+    @Test
+    public void testReplaceSymbolsShouldReplaceKthSymbol() {
+        StringMethods replace = new StringMethods();
+
+        String result = replace.replaceLetter(TEST_REPLACE_K);
+
+        Assert.assertEquals(EXPECTED_RK, result);
     }
 }
